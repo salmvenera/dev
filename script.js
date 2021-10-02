81a60b4b79d368b9385f7f8a026410fe2934f9a9
salmvenera <ahmetovavm@gmail.com>
2021-10-02 20:41:49 +0500

    lesson02

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let  fullPrice;
-let  adaptive;
+let title = 'Project';
+console.log(typeof title);  //вывод в консоль тип данных переменной //
 
-alert('Тест1');
-console.log('Тест2');
+let screens = 'Простые, сложные,интерактивные';
+console.log(screens.length);
+console.log(screens.toLowerCase());    //ставим (), чтобы вызвать метод, а не функцию//
+console.log(screens.split(','));
+
+let screenPrice = 'Стоимость верстки экранов 10 долларов';
+console.log(screenPrice.slice(0));
+
+let rollback = 100;
+
+let fullPrice = 'Стоимость разработки сайта 25555 рублей';
+console.log(fullPrice.slice(0));
+
+console.log(typeof fullPrice);   //вывод в консоль тип данных переменной //
+let adaptive = true;
+
+console.log(typeof adaptive);   //вывод в консоль тип данных переменной //
+
+let a = 25555, b = 100;
+console.log((a*(b/100)));
+  
+
+
+
 
