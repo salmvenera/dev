16c5bbccc8d668f370b6f3c26f27d3f89ca117aa
salmvenera <ahmetovavm@gmail.com>
2021-10-03 00:35:57 +0500

    исправление вывода в консоль текста и сумм

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ console.log(screens.length);
 console.log(screens.toLowerCase());    //ставим (), чтобы вызвать метод, а не функцию//
 console.log(screens.split(','));
 
-let screenPrice = 'Стоимость верстки экранов 10 долларов';
-console.log(screenPrice.slice(0));
+let screenPrice = 10;
+console.log(`Стоимость верстки экранов ${screenPrice}`);
 
 let rollback = 100;
 
-let fullPrice = 'Стоимость разработки сайта 25555 рублей';
-console.log(fullPrice.slice(0));
+let fullPrice = 25555;
+console.log(`Стоимость разработки сайта ${fullPrice}`);
+console.log(`Процент отката посреднику за работу ${(fullPrice * (rollback/100))}`);
 
 console.log(typeof fullPrice);   //вывод в консоль тип данных переменной //
 let adaptive = true;
 
 console.log(typeof adaptive);   //вывод в консоль тип данных переменной //
 
-let a = 25555, b = 100;
-console.log((a*(b/100)));
+
   
 
 
